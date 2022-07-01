8a7c0a93268c8b465616c3f9fd3ab0ca930e6966
kdjordan <k.dean.jordan@gmail.com>
2022-06-30 18:20:37 -0700

    added code comments for sql.js

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,9 +1,18 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/****************************************************************** 
+ * Helper function that parses an object containing update information and sniffs out what is present. 
+ * 
+ * PARAMS :
+ * 1. dataToUpdate = object containing some update information - what is actually contained is a mystery.
+ *                    We use Object.keys to extract what properties are contained
+ * 2. jsToSql = We then take the keys established in (1) and map them into SQL 'code' using the data 
+ *              available in the dataToUpdate Object
+*******************************************************************/
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
+  // if our OBject contains no data then throw an error
   if (keys.length === 0) throw new BadRequestError("No data");
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
